e8437de619a6ff7396f45942bdecfabaf4ceed86
Cristianvia <cristianviar@gmail.com>
2021-11-15 20:55:04 +0100

    Add data into js constant

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const avatarData = [
+    {
+        id: 1,
+        name: "Cristian",
+        img: "https://i.ibb.co/HCZ0McD/chibi-20211115-160456.png"
+    },
+    {
+        id: 2,
+        name: "Test",
+        img: "https://i.ibb.co/HCZ0McD/chibi-20211115-160456.png"
+    },
+];
