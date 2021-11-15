c7e9f79f26446e75ec95141f27b57b5612229f8c
Cristianvia <cristianviar@gmail.com>
2021-11-15 22:21:38 +0100

    Add localstorage filtered by id

--- src/Components/Avatar/Avatar.js
+++ src/Components/Avatar/Avatar.js
@@ -1,15 +1,26 @@
 import "./styles.css";
+import React, { useState, useEffect } from 'react';
 
 function Avatar(props) {
+    //Specify that the localstorage key equals the data id
+    const key = props.id;
+
+    const [count, setCount] = useState(() => {
+        const persistedValue = window.localStorage.getItem(key);
+        return persistedValue !== null ? JSON.parse(persistedValue) : 0;
+    });
+
+    useEffect(() => {
+        window.localStorage.setItem(key, JSON.stringify(count));
+    }, [count]);
+
     return (
         <li id={props.id}>
-            <img src={props.img} width="250" alt=""/>
+            <img src={props.img} width="250" alt="Avatar" />
             <span>{props.name}</span>
-            <span>{props.points}</span>
-            <div class="qty-holder">
-                <button class="qty-dec">-</button>
-                <input class="qty-adj" type="number" value="1" />
-                <button class="qty-inc">+</button></div>
+            <span>{count}</span>
+            <button class="subtract-count" onClick={() => setCount(count - 1)}>-</button>&nbsp;
+            <button class="add-count" onClick={() => setCount(count + 1)}>+</button>
         </li>
     );
 }
