63b94172d11a8d85d8a4238db67c5e73f18943fd
Cristianvia <cristianviar@gmail.com>
2021-11-15 22:52:41 +0100

    Add surname data

--- src/data.js
+++ src/data.js
@@ -2,11 +2,13 @@ export const avatarData = [
     {
         id: 1,
         name: "Cristian",
+        surname: "Viaplana",
         img: "../../images/1.png"
     },
     {
         id: 2,
         name: "Test",
+        surname: "Tester",
         img: "../../images/1.png"
     },
 ];
