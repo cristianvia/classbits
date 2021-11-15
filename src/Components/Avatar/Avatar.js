433c72e50e5396e7f067c970a6704ffd1ee1dcd1
Cristianvia <cristianviar@gmail.com>
2021-11-15 21:43:00 +0100

    add points props in avatar component

--- src/Components/Avatar/Avatar.js
+++ src/Components/Avatar/Avatar.js
@@ -5,6 +5,7 @@ function Avatar(props) {
         <li id={props.id}>
             <img src={props.img} width="250" alt=""/>
             <span>{props.name}</span>
+            <span>{props.points}</span>
             <div class="qty-holder">
                 <button class="qty-dec">-</button>
                 <input class="qty-adj" type="number" value="1" />
