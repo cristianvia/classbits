147f4e82e1aa00bbaa435f1f8533eadf3322c1bb
Cristianvia <cristianviar@gmail.com>
2021-11-16 14:46:26 +0100

    Add classroom picture

--- src/data.js
+++ src/data.js
@@ -1,12 +1,18 @@
 export const avatarData = [
     {
         id: 1,
+        name: "Tota la classe",
+        surname: "",
+        img: "../../images/chibi_classroom.png"
+    },
+    {
+        id: 2,
         name: "Cristian",
         surname: "Viaplana",
         img: "../../images/1.png"
     },
     {
-        id: 2,
+        id: 3,
         name: "Test",
         surname: "Tester",
         img: "../../images/1.png"
