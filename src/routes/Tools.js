11dcee94a47b66f180eea43a8b0cf99d4fae6aab
Cristianvia <cristianviar@gmail.com>
2021-11-17 22:34:44 +0100

    Create tools visual prototype

--- src/routes/Tools.js
+++ src/routes/Tools.js
@@ -4,8 +4,19 @@ export default function Tools() {
     return (
       <>
       <Header />
-      <main>
-      </main>
+      <div style={{ padding: 50 }}>
+          <div class="container">
+            <div class="cardDescription">
+                Medidor de soroll
+            </div>
+            <div class="cardDescription">
+                Frase diària
+            </div>
+            <div class="cardDescription">
+                Pergamí del destí
+            </div>
+          </div>
+        </div>
       </>
     );
   }
