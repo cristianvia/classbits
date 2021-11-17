0e296d9491d502e2bbb490e904f72be174664d8c
Cristianvia <cristianviar@gmail.com>
2021-11-17 14:20:51 +0100

    Change routes names

---
+++ src/routes/ToolsRoute.js
@@ -0,0 +1,11 @@
+import Header from '../Components/Header/Header';
+
+export default function ToolsRoute() {
+    return (
+      <>
+      <Header />
+      <main>
+      </main>
+      </>
+    );
+  }
