f01cf112d3f5f685f08a261ab34229d55670793b
cristianvia <cristianviar@gmail.com>
2021-11-17 16:00:46 +0100

    Create two columns

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,5 @@
 import Header from '../Components/Header/Header';
+import './HomeStyles.css';
 
 export default function Home() {
     return (
@@ -6,7 +7,10 @@ export default function Home() {
       <Header />
       <main>
       <div style={{padding:50}}>
-              2 columnes => Explicació sobre ClassBits + imatges
+      <div class="row">
+  <div class="column">Explicació ClassBits amb slogan catchy</div>
+  <div class="column">Foto</div>
+</div>
           </div>
           <div style={{padding:50}}>
               2 quadrats amb sobra gris (reutilitzar rtic)=> 1- Generador d'avatars 2-Joc
