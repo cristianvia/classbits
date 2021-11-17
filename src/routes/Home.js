9840505bc2d0ae1bda0f3a4143dce5967434217f
Cristianvia <cristianviar@gmail.com>
2021-11-17 22:36:06 +0100

    Add home steps to start playing and its styles

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,44 @@
 import Header from '../Components/Header/Header';
 import './HomeStyles.css';
+import { Link } from "react-router-dom";
 
 export default function Home() {
-    return (
-      <>
+  return (
+    <>
       <Header />
       <main>
-      <div style={{padding:50}}>
-      <div class="row">
-  <div class="column">Explicació ClassBits amb slogan catchy</div>
-  <div class="column">Foto</div>
-</div>
+        <div style={{ padding: 50 }}>
+          <div class="row">
+            <div class="column">
+              <p class="title">Comença l'experiència ClassBits</p>
+              <p class="subtitle">Crea el teu <b>ChiBit</b> al teu gust i comença a jugar ara mateix! Recorda: quants més punts aconsegueixis, a més premis podràs optar!</p>
+            </div>
+            <div class="column" id="chibiClassroom">
+              <div class="upAndDown">
+                <img src="../../images/chibi_classroom.png" alt="clase classbits" width="500px" />
+              </div>
+            </div>
           </div>
-          <div style={{padding:50}}>
-              2 quadrats amb sobra gris (reutilitzar rtic)=> 1- Generador d'avatars 2-Joc
+        </div>
+        <div style={{ marginLeft: 50 }}>
+          <div class="container">
+            <div class="cardDescription">
+              <a href="https://charat.me/en/chibi/create/" target="_blank">
+                Crea el teu Chibit
+                <img src="../../images/chibi_potter.png" alt="chibi potter classbits" width="150" />
+              </a>
+              <div class="steps">1</div>
+            </div>
+            <div class="cardDescription">
+              <Link to="/game">
+                Comença a jugar
+                <img src="../../images/chibi_ironman.png" alt="chibi potter classbits" width="150" />
+                <div class="steps">2</div>
+              </Link>
+            </div>
           </div>
+        </div>
       </main>
-      </>
-    );
-  }
+    </>
+  );
+}
