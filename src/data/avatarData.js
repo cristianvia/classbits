7cc0824e2ddff44346e83f231ae372433e1f5c30
cristianvia <cristianviar@gmail.com>
2021-11-18 16:32:21 +0100

    Fill data js

--- src/data/avatarData.js
+++ src/data/avatarData.js
@@ -7,14 +7,170 @@ export const avatarData = [
     },
     {
         id: 1,
-        name: "Cristian",
-        surname: "Viaplana",
+        name: "Marc",
+        surname: "Aguado",
         img: "../../images/1.png"
     },
     {
         id: 2,
-        name: "Test",
-        surname: "Tester",
-        img: "../../images/1.png"
+        name: "Alba",
+        surname: "Alabart",
+        img: "../../images/2.png"
+    },
+    {
+        id: 3,
+        name: "Adriana",
+        surname: "Bertran",
+        img: "../../images/3.png"
+    },
+    {
+        id: 4,
+        name: "Guillem",
+        surname: "Bonet",
+        img: "../../images/4.png"
+    },
+    {
+        id: 5,
+        name: "Adrià",
+        surname: "Calvís",
+        img: "../../images/5.png"
+    },
+    {
+        id: 6,
+        name: "Àlex",
+        surname: "Càmara",
+        img: "../../images/6.png"
+    },
+    {
+        id: 7,
+        name: "Aitana",
+        surname: "Carmona",
+        img: "../../images/7.png"
+    },
+    {
+        id: 8,
+        name: "Pau",
+        surname: "Domingo",
+        img: "../../images/8.png"
+    },
+    {
+        id: 9,
+        name: "Aina",
+        surname: "Farrús",
+        img: "../../images/9.png"
+    },
+    {
+        id: 10,
+        name: "Sara",
+        surname: "Garcia",
+        img: "../../images/10.png"
+    },
+    {
+        id: 11,
+        name: "Clàudia",
+        surname: "Guerrero",
+        img: "../../images/11.png"
+    },
+    {
+        id: 12,
+        name: "Emma",
+        surname: "Incani",
+        img: "../../images/12.png"
+    },
+    {
+        id: 13,
+        name: "Clara",
+        surname: "Jené",
+        img: "../../images/13.png"
+    },
+    {
+        id: 14,
+        name: "Naima",
+        surname: "Jiménez",
+        img: "../../images/14.png"
+    },
+    {
+        id: 15,
+        name: "Arnau",
+        surname: "Llurba",
+        img: "../../images/15.png"
+    },
+    {
+        id: 16,
+        name: "Alba",
+        surname: "Mariazcurrena",
+        img: "../../images/16.png"
+    },
+    {
+        id: 17,
+        name: "Àlex",
+        surname: "Melero",
+        img: "../../images/17.png"
+    },
+    {
+        id: 18,
+        name: "Àngels",
+        surname: "Minguella",
+        img: "../../images/18.png"
+    },
+    {
+        id: 19,
+        name: "Pablo",
+        surname: "Moreno",
+        img: "../../images/19.png"
+    },
+    {
+        id: 20,
+        name: "Ares",
+        surname: "Pascual",
+        img: "../../images/20.png"
+    },
+    {
+        id: 21,
+        name: "Salma",
+        surname: "Radi",
+        img: "../../images/21.png"
+    },
+    {
+        id: 22,
+        name: "Adrià",
+        surname: "Rius",
+        img: "../../images/22.png"
+    },
+    {
+        id: 23,
+        name: "David",
+        surname: "Sandoval",
+        img: "../../images/23.png"
+    },
+    {
+        id: 24,
+        name: "Bàrbara",
+        surname: "Tarragó",
+        img: "../../images/24.png"
+    },
+    {
+        id: 25,
+        name: "Júlia",
+        surname: "Tarridas",
+        img: "../../images/25.png"
+    },
+    {
+        id: 26,
+        name: "Carla",
+        surname: "Tersa",
+        img: "../../images/26.png"
+    },
+    {
+        id: 27,
+        name: "Pau",
+        surname: "Vidal",
+        img: "../../images/27.png"
+    },
+    {
+        id: 28,
+        name: "Yago",
+        surname: "Vidal",
+        img: "../../images/28.png"
     },
 ];
