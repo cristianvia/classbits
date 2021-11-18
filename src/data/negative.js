c758709680d404fd4fe00acd5fbfcdbfbabdef29
Cristianvia <cristianviar@gmail.com>
2021-11-18 23:25:59 +0100

    Create positive and negative json

---
+++ src/data/negative.js
@@ -0,0 +1,48 @@
+export const negativeData = [
+    {
+        id: 0,
+        name: "Faltar el respecte a un company",
+        emoji: ""
+    },
+    {
+        id: 1,
+        name: "Molestar als companys",
+        emoji: ""
+    },
+    {
+        id: 2,
+        name: "No estar atent",
+        emoji: ""
+    },
+    {
+        id: 3,
+        name: "No fer els deures",
+        emoji: ""
+    },
+    {
+        id: 4,
+        name: "No fer silenci",
+        emoji: ""
+    },
+    {
+        id: 5,
+        name: "No treballar",
+        emoji: ""
+    },
+    {
+        id: 6,
+        name: "Pegar-se amb un company",
+        emoji: ""
+    },
+    {
+        id: 7,
+        name: "No fer cas del mestre/a",
+        emoji: ""
+    },
+    {
+        id: 8,
+        name: "Balancejar-se a la cadira",
+        emoji: ""
+    },
+
+];
---
+++ src/data/positive.js
@@ -0,0 +1,48 @@
+export const positiveData = [
+    {
+        id: 0,
+        name: "Ajuda els altres",
+        emoji: ""
+    },
+    {
+        id: 1,
+        name: "Endreça i ajuda a endreçar",
+        emoji: ""
+    },
+    {
+        id: 2,
+        name: "Està per la feina",
+        emoji: ""
+    },
+    {
+        id: 3,
+        name: "Fer l'encàrrec molt bé",
+        emoji: ""
+    },
+    {
+        id: 4,
+        name: "Participa",
+        emoji: ""
+    },
+    {
+        id: 5,
+        name: "Pergamí del destí",
+        emoji: ""
+    },
+    {
+        id: 6,
+        name: "S'esforça molt",
+        emoji: ""
+    },
+    {
+        id: 7,
+        name: "Treballa en equip",
+        emoji: ""
+    },
+    {
+        id: 8,
+        name: "Treure un excel·lent",
+        emoji: ""
+    },
+
+];
