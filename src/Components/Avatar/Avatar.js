06cb4fd76f7567cad4584fea50351397c6f75fcb
Cristianvia <cristianviar@gmail.com>
2021-11-18 23:25:31 +0100

    Add Tabs component to Avatar Modal

--- src/Components/Avatar/Avatar.js
+++ src/Components/Avatar/Avatar.js
@@ -1,38 +1,40 @@
 import "./styles.css";
 import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
+import Tabs from "../Tab/Tabs";
 
-const customStyles = {
+const modalStyles = {
     content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
     },
-  };
-  
-  // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
-  Modal.setAppElement('#root');
+};
+
+// Make sure to bind modal to appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#root');
 
 function Avatar(props) {
 
     let subtitle;
-  const [modalIsOpen, setIsOpen] = React.useState(false);
+    const [modalIsOpen, setIsOpen] = React.useState(false);
 
-  function openModal() {
-    setIsOpen(true);
-  }
+    function openModal() {
+        setIsOpen(true);
+    }
 
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
-  }
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+    }
 
-  function closeModal() {
-    setIsOpen(false);
-  }
+    function closeModal() {
+        setIsOpen(false);
+    }
+
 
     //Specify that the localstorage key equals the data id
     const key = props.id;
@@ -47,42 +49,49 @@ function Avatar(props) {
     }, [count]);
 
     const substractAll = () => {
-        Array.from(document.querySelectorAll(".subtract-count")).forEach(button=>button.click())
-      }
-    
-      const addAll = () => {
-        Array.from(document.querySelectorAll(".add-count")).forEach(button=>button.click())
-      }
+        Array.from(document.querySelectorAll(".subtract-count")).forEach(button => button.click())
+    }
+
+    const addAll = () => {
+        Array.from(document.querySelectorAll(".add-count")).forEach(button => button.click())
+    }
 
     return (
         <>
-        <li id={props.id}>
-            <span className={(count > 0) ? "circleGreen" : (count < 0) ? "circleRed" : "circle"}>{(props.id == 0) ? "" : count}</span>
-            <img src={props.img} width="250" alt="Avatar" />
-            <span class="name">{props.name}</span>
-            <span class="surname">{props.surname}</span>
-            <button class="subtract-count" onClick={() => (props.id == 0) ? (substractAll(), setCount(count - 1)) : setCount(count - 1)}>-</button>&nbsp;
-            <button class="add-count" onClick={() => (props.id == 0) ? (addAll(), setCount(count + 1)) : setCount(count + 1)}>+</button>
-        </li>
-        <button onClick={openModal}>Open Modal</button>
-      <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
-        <button onClick={closeModal}>close</button>
-        <div>I am a modal</div>
-        <form>
-          <input />
-          <button>{props.name}</button>
-          <button>stays</button>
-          <button>inside</button>
-          <button>the modal</button>
-        </form>
-      </Modal>
+            <li id={props.id}>
+                <span className={(count > 0) ? "circleGreen" : (count < 0) ? "circleRed" : "circle"}>{(props.id == 0) ? "" : count}</span>
+                <img src={props.img} width="250" alt="Avatar" />
+                <span class="name">{props.name}</span>
+                <span class="surname">{props.surname}</span>
+                <button class="subtract-count" onClick={() => (props.id == 0) ? (substractAll(), setCount(count - 1)) : setCount(count - 1)}>-</button>&nbsp;
+                <button class="add-count" onClick={() => (props.id == 0) ? (addAll(), setCount(count + 1)) : setCount(count + 1)}>+</button>
+            </li>
+            <button onClick={openModal}>Open Modal</button>
+            <Modal
+                isOpen={modalIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={modalStyles}
+                contentLabel="Modal"
+            >
+                <h2 ref={(_subtitle) => (subtitle = _subtitle)}>{props.name} {props.surname}</h2>
+
+                <div class="container">
+                    <img src={props.img} width="450" alt="Avatar" />
+                    <Tabs>
+                        <div label="Gator">
+                            See ya later, <em>Alligator</em>!
+                        </div>
+                        <div label="Croc">
+                            After 'while, <em>Crocodile</em>!
+                        </div>
+                        <div label="Sarcosuchus">
+                            Nothing to see here, this tab is <em>extinct</em>!
+                        </div>
+                    </Tabs>
+                </div>
+                <button onClick={closeModal}>close</button>
+            </Modal>
         </>
     );
 }
