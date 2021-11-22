7386239835e09396b8a2edb9494e20298093d071
Cristianvia <cristianviar@gmail.com>
2021-11-23 00:09:57 +0100

    Generate random groups with nice postit style

--- src/routes/Groups.js
+++ src/routes/Groups.js
@@ -21,6 +21,9 @@ export default function Groups() {
 
   const createGroups = () => {
     var number = document.getElementById("number").value;
+    if (number == 0){
+      number = 2
+    }
     var name_list = getArrayChibits();
     var group = [];
 
@@ -47,7 +50,7 @@ export default function Groups() {
     for (var i = 0; i < group.length; i++) {
       htmlresult += "<div class='avatarGroups'><div style='text-align:center;'><strong> Grupo " + (i + 1) + "</strong></div><ul>";
       for (var j = 0; j < group[i].length; j++) {
-        htmlresult += "<li>" + group[i][j] + "</li>";
+        htmlresult += "<li>" + group[i][j] + "</li><br>";
       }
       htmlresult += "</ul></div>";
     }
@@ -55,23 +58,16 @@ export default function Groups() {
     document.getElementById("result").innerHTML = htmlresult;
   }
 
-
-
-
-
-
-
-
-
-
-
   return (
     <>
       <Header />
       <div style={{ padding: 50 }}>
         <div class="container">
-          <p><span id="cantidadGrupos">Cantidad de grupos: </span><input id="number"></input></p>
+          <div>
+          <p><span id="cantidadGrupos">Cantidad de grupos: </span><input id="number" size="2"></input></p>
           <button class="createGroupsButton" onClick={createGroups}> Crear grupos </button>
+          </div>
+          
           <div id="result"></div>
         </div>
       </div>
