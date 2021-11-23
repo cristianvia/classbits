8160bcc68159fe5163bfc0c11efa153168a4b946
Cristianvia <cristianviar@gmail.com>
2021-11-23 15:12:03 +0100

    Add visual tools

--- src/routes/Tools.js
+++ src/routes/Tools.js
@@ -1,24 +1,30 @@
 import Header from '../Components/Header/Header';
+import "./ToolsStyles.css";
 import { Link } from "react-router-dom";
 
 
 export default function Tools() {
-    return (
-      <>
+  return (
+    <>
       <Header />
       <div style={{ padding: 50 }}>
-          <div class="container">
-            <div class="cardDescription">
-                Medidor de soroll
-            </div>
-            <div class="cardDescription">
-                Frase diària
-            </div>
-            <div class="cardDescription">
-            <Link to="/pergamino">Pergamí del destí</Link>
-            </div>
+        <div class="container toolsContainer">
+          <div>
+            <img src="../../images/tools/semaforo.png" alt="Medidor de ruido" class="toolsCircle" />
+            <figcaption class="textCaption">Medidor de soroll</figcaption>
+          </div>
+          <div>
+            <img src="../../images/tools/frase_diaria.png" alt="Frase diaria" class="toolsCircle" />
+            <figcaption class="textCaption">Frase diària</figcaption>
+          </div>
+          <div>
+          <Link to="/pergamino">
+          <img src="../../images/tools/pergamino.png" alt="Frase diaria" class="toolsCircle" />
+          </Link>
+           <figcaption class="textCaption">Pergamí del destí</figcaption>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+}
