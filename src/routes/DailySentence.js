4b550cfb1afd094d46af03cf6f710a5b1f07de8e
cristianvia <cristianviar@gmail.com>
2021-11-25 16:41:14 +0100

    Remove unneeded code

--- src/routes/DailySentence.js
+++ src/routes/DailySentence.js
@@ -4,18 +4,6 @@ import dailySentence from "../data/dailySentenceData.json";
 
 export default function DailySentence() {
     var myArray = dailySentence;
-    function outPutOptions(arr) {
-        var output = '';
-        var length = arr.length;
-        for (var x = 0; x < length; x++) {
-            if (length - 1 === x) {
-                output += arr[x] + '';
-            } else {
-                output += arr[x] + ', ';
-            }
-        }
-        return output;
-    }
 
     function getRandom() {
         return myArray[Math.floor(Math.random() * myArray.length)];
@@ -30,7 +18,7 @@ export default function DailySentence() {
             <Header />
             <main>
                 <div style={{ padding: 70 }}>
-                    <button class="createGroupsButton" type="button" onClick={getSentence}>Dime una frase bonita</button>
+                    <button class="createGroupsButton" type="button" onClick={getSentence}>Genera una frase bonica</button>
                     <div className="sentenceContainer">
                         <img src="/images/chibi_yoda.png" alt="yoda" width="350px" height="300px"/>
                         <div class="fraseBonitaContainer">
