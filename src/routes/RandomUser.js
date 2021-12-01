72514db375556a57688afdf0a1c79cd590ddf4fc
cristianvia <cristianviar@gmail.com>
2021-12-01 10:43:52 +0100

    Redo

--- src/routes/RandomUser.js
+++ src/routes/RandomUser.js
@@ -2,16 +2,15 @@ import Header from '../Components/Header/Header';
 import './RandomUserStyles.css';
 import avatarData from "../data/avatarData.json";
 import Avatar from '../Components/Avatar/Avatar';
-import React, { useState, useEffect } from 'react/cjs/react.development';
+import React, { useState } from 'react/cjs/react.development';
 
 
 export default function RandomUser() {
-var randomAvatar = avatarData[0];
+var randomAvatar = [];
 const [chosenAvatar, setChosenAvatar] = useState([]);
 
     const changeState = () => {
-        setChosenAvatar(Math.floor(Math.random() * (30 - 1) + 1))
-        console.log(chosenAvatar)
+        setChosenAvatar([""])
     }
 
     const getRandomAvatar = () => {
