b68f1d8b4944f32a555fb2a5314ced7c5e0db439
cristianvia <cristianviar@gmail.com>
2022-01-11 16:14:50 +0100

    Create addEdit component

---
+++ src/routes/AddEdit.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import { useHistory, useParams } from "react-router-dom";
+
+const AddEdit = () => {
+    return (
+        <div>
+            <h2>Afegir alumne:</h2>
+        </div>
+    )
+}
+
+export default AddEdit;
