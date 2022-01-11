bcdc08a3658f75be965da4c516a9e485d46ab4c6
cristianvia <cristianviar@gmail.com>
2022-01-11 17:17:09 +0100

    Add student form and styling

--- src/routes/AddEdit.js
+++ src/routes/AddEdit.js
@@ -1,10 +1,66 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory, useParams } from "react-router-dom";
+import "./AddEdit.css";
+import fireDb from "../firebaseConfig";
+import { toast } from "react-toastify";
+
+import Header from '../Components/Header/Header';
+import StudentHeader from "../Components/StudentHeader/StudentHeader"
+
+const initialState = {
+    name: "",
+    surname: "",
+    img: "",
+    points: 0
+}
+
+const handleInputChange = () => { };
+
+const handleSubmit = () => { };
 
 const AddEdit = () => {
+    const [state, setState] = useState(initialState);
+    const [data, setData] = useState({});
+
+    const { name, surname, img, points } = state;
     return (
         <div>
+            <Header />
+            <StudentHeader />
             <h2>Afegir alumne:</h2>
+            <form style={{
+                margin: "auto",
+                padding: "15px",
+                maxWidth: "400px",
+                alignContent: "center"
+            }}
+                onSubmit={handleSubmit}>
+                <label htmlFor="name">Nom</label>
+                <input
+                    type="text"
+                    id="name"
+                    placeHolder="Alumne"
+                    value={name}
+                    onChange={handleInputChange} />
+
+                <label htmlFor="surname">Cognom</label>
+                <input
+                    type="text"
+                    id="surname"
+                    placeHolder="Cognom"
+                    value={surname}
+                    onChange={handleInputChange} />
+
+                <label htmlFor="img">ChiBit</label>
+                <input
+                    type="text"
+                    id="img"
+                    placeHolder="Imatge"
+                    value={img}
+                    onChange={handleInputChange} />
+
+                <input type="submit" value="Guardar" />
+            </form>
         </div>
     )
 }
