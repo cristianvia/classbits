46dc9b51089abd775e1e9ca6cb792ab25f5d7453
cristianvia <cristianviar@gmail.com>
2022-01-14 22:52:57 +0100

    Create howto route

---
+++ src/routes/HowTo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Components/Header/Header';
+import StudentHeader from "../Components/StudentHeader/StudentHeader"
+
+
+const HowTo = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default HowTo
