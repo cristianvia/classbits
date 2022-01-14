f41eb1c206e4acb7977607aa0d9eb0bdc455d569
cristianvia <cristianviar@gmail.com>
2022-01-14 11:50:28 +0100

    Fix add student header

--- src/Components/StudentHeader/StudentHeader.js
+++ src/Components/StudentHeader/StudentHeader.js
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom";
 
 const StudentHeader = () => {
     return (
-        <header>
-            <nav>
+        <div className="headerSH">
+            <div className="navSH">
                 <div>
                     <Link to="/add">Afegir alumne</Link>
-                    <Link to="/update/:id">Actualitzar alumne</Link>
+                    {/* <Link to="/update/:id">Actualitzar alumne</Link> */}
                     {/* <Link to="/view/:id">Veure</Link> */}
                 </div>
-            </nav>
-        </header>
+            </div>
+        </div>
     )
 }
 
