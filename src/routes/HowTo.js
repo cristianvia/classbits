12b467000ae6461d16cb2e6acc035a3a04642408
cristianvia <cristianviar@gmail.com>
2022-01-14 22:56:16 +0100

    First howto text created

--- src/routes/HowTo.js
+++ src/routes/HowTo.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from '../Components/Header/Header';
 import StudentHeader from "../Components/StudentHeader/StudentHeader"
+import { Link } from "react-router-dom";
 
 
 const HowTo = () => {
     return (
         <div>
             <Header />
+            <div>
+                Ara que ja heu creat tots els ChiBits només queden 2 passos:
+                1. Comprimir les imatges arrossegant-les de 10 en 10 en <a href="https://compressor.io" target="_blank">aquest web</a>.
+                2. Crear cada alumne dins de la secció <Link to="/add">classbits</Link>
+            </div>
         </div>
     )
 }
