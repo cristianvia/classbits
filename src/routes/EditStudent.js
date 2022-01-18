40095f9d2dbe88933c0fda1332ca28ffaa41a105
Cristianvia <cristianviar@gmail.com>
2022-01-18 22:07:03 +0100

    Add logic to update students

--- src/routes/EditStudent.js
+++ src/routes/EditStudent.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from "react-router-dom";
 import "./AddEdit.css";
 
@@ -6,16 +6,102 @@ import Header from '../Components/Header/Header';
 import StudentHeader from "../Components/StudentHeader/StudentHeader"
 import imageCompression from 'browser-image-compression';
 
+var imageCompressed = "";
+var finalImageCompressed = "";
+var classroom = JSON.parse(localStorage.getItem("classroom") || []);
+var idStudent = "";
+
+function UpdateDataToLocalStorage(newClassroomEdited) {
+    
+    classroom[idStudent].name = newClassroomEdited.name;
+    classroom[idStudent].surname = newClassroomEdited.surname;
+    classroom[idStudent].img = newClassroomEdited.img;
+
+    // Re-serialize the array back into a string and store it in localStorage
+    localStorage.setItem('classroom', JSON.stringify(classroom));
+}
+
+function setImageCompressed(e) {
+    imageCompressed = e;
+    console.log("setImageCompressed", e)
+}
+
+//the compressed image is returned in blob format, needs to be saved as base64
+const blobToBase64 = blob => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    return new Promise(resolve => {
+        reader.onloadend = () => {
+            resolve(reader.result);
+        };
+    });
+};
+
 const EditStudent = () => {
 
-    const {id} = useParams();
+    const { id } = useParams();
+    idStudent = id;
     console.log(id)
+
+    async function handleImageUpload(event) {
+
+        const imageFile = event.target.files[0];
+        console.log('originalFile instanceof Blob', imageFile instanceof Blob); // true
+        console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
+
+        const options = {
+            maxSizeMB: 1,
+            maxWidthOrHeight: 500,
+            useWebWorker: true
+        }
+        try {
+            const compressedFile = await imageCompression(imageFile, options);
+            console.log('compressedFile instanceof Blob', compressedFile instanceof Blob); // true
+            console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
+
+            await setImageCompressed(compressedFile);
+
+            blobToBase64(imageCompressed).then(res => {
+                //final image compressed and converted into base64
+                finalImageCompressed = res;
+            });
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    useEffect(() => {
+
+        const formElement = document.getElementById("addStudentForm");
+
+        const handleForm = async (event) => {
+            event.preventDefault();
+
+            const formData = new FormData(formElement);
+
+            let data = {
+                id: id,
+                name: formData.get("name"),
+                surname: formData.get("surname"),
+                img: finalImageCompressed
+            };
+
+            UpdateDataToLocalStorage(data);
+            alert("Usuari afegit correctament")
+            setTimeout(() => window.location.reload(), 500)
+        };
+
+        formElement.addEventListener("submit", handleForm);
+
+    }, []);
+
 
     return (
         <div>
             <Header />
             <StudentHeader />
-            <h2 style={{ textAlign: "center" }}>Editar alumne</h2>
+            <h2 style={{ textAlign: "center" }}>Editar {classroom[id].name} {classroom[id].surname} </h2>
             <form id="addStudentForm"
                 style={{
                     margin: "auto",
@@ -37,10 +123,10 @@ const EditStudent = () => {
                 <input type="text" id="surnameInput" name="surname" />
 
                 <label htmlFor="pictureInput">Imatge</label>
-                <input type="file" id="pictureInput" name="img" 
-                multiple 
-                accept="image/*"
-                // onChange={event => handleImageUpload(event)}
+                <input type="file" id="pictureInput" name="img"
+                    multiple
+                    accept="image/*"
+                    onChange={event => handleImageUpload(event)}
                 />
 
                 <input type="submit" value="Guardar" />
