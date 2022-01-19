ff7625d72dc66fdb6c077dc8202e8396d40802af
cristianvia <cristianviar@gmail.com>
2022-01-19 12:05:00 +0100

    Create positive in ls if doesnt exist

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './Components/Header/Header';
 
 
 //Check if localstorage classroom exists, otherwise create it
-const checkIfLSexists = () => {
+const checkIfClassroomLsExist = () => {
   var checkLs = JSON.parse(localStorage.getItem("classroom"))
   if (checkLs == null){
     localStorage.setItem("classroom", JSON.stringify([{
@@ -16,8 +16,21 @@ const checkIfLSexists = () => {
     }]));
   }
 }
+//Check if localstorage positive exists, otherwise create it
+const checkIfPositiveLsExist = () => {
+  var checkPositive = JSON.parse(localStorage.getItem("positive"))
+  if (checkPositive == null){
+    localStorage.setItem("positive", JSON.stringify([{
+      "id": 0,
+      "name": "Estar atent",
+      "emoji": "📦",
+      "points": 1
+    }]));
+  }
+}
+checkIfClassroomLsExist();
+checkIfPositiveLsExist();
 
-checkIfLSexists();
 
 var classroom = JSON.parse(localStorage.getItem("classroom") || []);
 
