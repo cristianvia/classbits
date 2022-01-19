4185b408020120c8f2bf77e9ed6a46cf6dd6a54a
Cristianvia <cristianviar@gmail.com>
2022-01-19 09:49:08 +0100

    Add rules settings button

--- src/routes/Settings.js
+++ src/routes/Settings.js
@@ -19,7 +19,9 @@ const Settings = () => {
     return (
         <div>
             <Header />
+
             <div className="settings">
+            <p>Configuració de classe</p>
                 <p><Link to="/add">
                     <button className="button-success">Afegir alumnes</button>
                 </Link></p>
@@ -27,7 +29,28 @@ const Settings = () => {
                     <button className="button-secondary">Editar alumnes</button>
                 </Link></p>
                 <p><button className="button-error" onClick={deleteClassroom}>Borrar classe</button></p>
-
+            </div>
+            {/* Rules */}
+            <div className="settings">
+                <p>Configuració de normes</p>
+                <p><Link to="/add">
+                    <button className="button-success">Afegir positius</button>
+                </Link></p>
+                <p><Link to="/add">
+                    <button className="button-secondary">Editar positius</button>
+                </Link></p>
+                <p><Link to="/add">
+                    <button className="button-success">Afegir negatius</button>
+                </Link></p>
+                <p><Link to="/add">
+                    <button className="button-secondary">Editar negatius</button>
+                </Link></p>
+                <p><Link to="/add">
+                    <button className="button-success">Afegir bescanvis</button>
+                </Link></p>
+                <p><Link to="/add">
+                    <button className="button-secondary">Editar bescanvis</button>
+                </Link></p>
             </div>
         </div>
     )
