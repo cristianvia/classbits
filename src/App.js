fca71a633c3df417f60ebe98e71bbd6f35cff143
Cristianvia <cristianviar@gmail.com>
2022-01-20 22:38:52 +0100

    Add pets to localstorage, if it doesn't exist

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './Components/Header/Header';
 //Check if localstorage classroom exists, otherwise create it
 const checkIfClassroomLsExist = () => {
   var checkLs = JSON.parse(localStorage.getItem("classroom"))
-  if (checkLs == null){
+  if (checkLs == null) {
     localStorage.setItem("classroom", JSON.stringify([{
       "id": 0,
       "name": "Tota la classe",
@@ -20,178 +20,224 @@ const checkIfClassroomLsExist = () => {
 //Check if localstorage positive exists, otherwise create it
 const checkIfPositiveLsExist = () => {
   var checkPositive = JSON.parse(localStorage.getItem("positive"))
-  if (checkPositive == null){
+  if (checkPositive == null) {
     localStorage.setItem("positive", JSON.stringify([
       {
-          "id": 1,
-          "name": "Endreça i ajuda a endreçar",
-          "emoji": "📦",
-          "points": 1
+        "id": 1,
+        "name": "Endreça i ajuda a endreçar",
+        "emoji": "📦",
+        "points": 1
       },
       {
-          "id": 2,
-          "name": "Està per la feina",
-          "emoji": "✏️",
-          "points": 1
+        "id": 2,
+        "name": "Està per la feina",
+        "emoji": "✏️",
+        "points": 1
       },
       {
-          "id": 3,
-          "name": "Fer l'encàrrec molt bé",
-          "emoji": "🎩",
-          "points": 1
+        "id": 3,
+        "name": "Fer l'encàrrec molt bé",
+        "emoji": "🎩",
+        "points": 1
       },
       {
-          "id": 4,
-          "name": "Participa",
-          "emoji": "🙋‍♂️",
-          "points": 1
+        "id": 4,
+        "name": "Participa",
+        "emoji": "🙋‍♂️",
+        "points": 1
       },
       {
-          "id": 5,
-          "name": "Pergamí del destí",
-          "emoji": "📜",
-          "points": 1
+        "id": 5,
+        "name": "Pergamí del destí",
+        "emoji": "📜",
+        "points": 1
       },
       {
-          "id": 6,
-          "name": "S'esforça molt",
-          "emoji": "💪",
-          "points": 1
+        "id": 6,
+        "name": "S'esforça molt",
+        "emoji": "💪",
+        "points": 1
       },
       {
-          "id": 7,
-          "name": "Treballa en equip",
-          "emoji": "🧑‍🤝‍🧑",
-          "points": 1
+        "id": 7,
+        "name": "Treballa en equip",
+        "emoji": "🧑‍🤝‍🧑",
+        "points": 1
       },
       {
-          "id": 8,
-          "name": "Treure un excel·lent",
-          "emoji": "💯",
-          "points": 2
+        "id": 8,
+        "name": "Treure un excel·lent",
+        "emoji": "💯",
+        "points": 2
       }
-  
-  ]));
+
+    ]));
   }
 }
 
 //Check if localstorage negative exists, otherwise create it
 const checkIfNegativeLsExist = () => {
   var checkNegative = JSON.parse(localStorage.getItem("negative"))
-  if (checkNegative == null){
+  if (checkNegative == null) {
     localStorage.setItem("negative", JSON.stringify([
-    
+
       {
-          "id": 1,
-          "name": "Molestar als companys",
-          "emoji": "😤",
-          "points": 1
+        "id": 1,
+        "name": "Molestar als companys",
+        "emoji": "😤",
+        "points": 1
       },
       {
-          "id": 2,
-          "name": "No estar atent",
-          "emoji": "🙄",
-          "points": 1
+        "id": 2,
+        "name": "No estar atent",
+        "emoji": "🙄",
+        "points": 1
       },
       {
-          "id": 3,
-          "name": "No fer els deures",
-          "emoji": "✍️",
-          "points": 1
+        "id": 3,
+        "name": "No fer els deures",
+        "emoji": "✍️",
+        "points": 1
       },
       {
-          "id": 4,
-          "name": "No fer silenci",
-          "emoji": "🤐",
-          "points": 1
+        "id": 4,
+        "name": "No fer silenci",
+        "emoji": "🤐",
+        "points": 1
       },
       {
-          "id": 5,
-          "name": "Balancejar-se a la cadira",
-          "emoji": "🪑",
-          "points": 1
+        "id": 5,
+        "name": "Balancejar-se a la cadira",
+        "emoji": "🪑",
+        "points": 1
       },
       {
-          "id": 6,
-          "name": "Faltar el respecte a un company",
-          "emoji": "🤬",
-          "points": 2
+        "id": 6,
+        "name": "Faltar el respecte a un company",
+        "emoji": "🤬",
+        "points": 2
       },
       {
-          "id": 7,
-          "name": "Pegar-se amb un company",
-          "emoji": "🤼",
-          "points": 4
+        "id": 7,
+        "name": "Pegar-se amb un company",
+        "emoji": "🤼",
+        "points": 4
       },
       {
-          "id": 8,
-          "name": "No fer cas del mestre/a",
-          "emoji": "👨‍🏫",
-          "points": 5
+        "id": 8,
+        "name": "No fer cas del mestre/a",
+        "emoji": "👨‍🏫",
+        "points": 5
       }
-  ]));
+    ]));
   }
 }
 
 //Check if localstorage positive exists, otherwise create it
 const checkIfExchangeLsExist = () => {
   var checkExchange = JSON.parse(localStorage.getItem("exchange"))
-  if (checkExchange == null){
+  if (checkExchange == null) {
     localStorage.setItem("exchange", JSON.stringify([
       {
-          "id": 1,
-          "name": "Triar una cançó",
-          "emoji": "🎶",
-          "points": 1
+        "id": 1,
+        "name": "Triar una cançó",
+        "emoji": "🎶",
+        "points": 1
       },
       {
-          "id": 2,
-          "name": "Corregir a la pissarra",
-          "emoji": "📝",
-          "points": 1
+        "id": 2,
+        "name": "Corregir a la pissarra",
+        "emoji": "📝",
+        "points": 1
       },
       {
-          "id": 3,
-          "name": "Anar al lavabo",
-          "emoji": "🚾",
-          "points": 1
+        "id": 3,
+        "name": "Anar al lavabo",
+        "emoji": "🚾",
+        "points": 1
       },
       {
-          "id": 4,
-          "name": "Anar el primer a la fila",
-          "emoji": "🥇",
-          "points": 5
+        "id": 4,
+        "name": "Anar el primer a la fila",
+        "emoji": "🥇",
+        "points": 5
       },
       {
-          "id": 5,
-          "name": "Canvi de lloc durant un dia",
-          "emoji": "🔄",
-          "points": 15
+        "id": 5,
+        "name": "Canvi de lloc durant un dia",
+        "emoji": "🔄",
+        "points": 15
       },
       {
-          "id": 6,
-          "name": "Mirar vídeos mentre esmorzem",
-          "emoji": "🎬",
-          "points": 10
+        "id": 6,
+        "name": "Mirar vídeos mentre esmorzem",
+        "emoji": "🎬",
+        "points": 10
       },
       {
-          "id": 7,
-          "name": "Asseure a la cadira del professor",
-          "emoji": "💺",
-          "points": 30
+        "id": 7,
+        "name": "Asseure a la cadira del professor",
+        "emoji": "💺",
+        "points": 30
       },
       {
-          "id": 8,
-          "name": "Escollir què farem a classe",
-          "emoji": "🪄",
-          "points": 99
-      }  
-  ]));
+        "id": 8,
+        "name": "Escollir què farem a classe",
+        "emoji": "🪄",
+        "points": 99
+      }
+    ]));
+  }
+}
+
+//Check if localstorage classroom exists, otherwise create it
+const checkIfPetsLsExist = () => {
+  var checkPetLs = JSON.parse(localStorage.getItem("pets"))
+  if (checkPetLs == null) {
+    localStorage.setItem("pets", JSON.stringify([
+      {
+        "id": 1,
+        "name": "Pikachu",
+        "img": "../../images/pets/chibi_pikachu.png",
+      },
+      {
+        "id": 2,
+        "name": "Bulbasaur",
+        "img": "../../images/pets/chibi_bulbasaur.png",
+      },
+      {
+        "id": 3,
+        "name": "Squirte",
+        "img": "../../images/pets/chibi_squirtle.png",
+      },
+      {
+        "id": 4,
+        "name": "Charmander",
+        "img": "../../images/pets/chibi_charmander.png",
+      },
+      {
+        "id": 5,
+        "name": "Doggy",
+        "img": "../../images/pets/chibi_dog.png",
+      },
+      {
+        "id": 6,
+        "name": "Foxy",
+        "img": "../../images/pets/chibi_fox.png",
+      },
+      {
+        "id": 7,
+        "name": "Corets",
+        "img": "../../images/pets/chibi_loving_dog.png",
+      },
+      
+    ]));
   }
 }
 
 
 checkIfClassroomLsExist();
+checkIfPetsLsExist();
 checkIfPositiveLsExist();
 checkIfNegativeLsExist();
 checkIfExchangeLsExist();
