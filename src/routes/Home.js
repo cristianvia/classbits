d75007bbdd7b3ab657b1edad658d30bf76b159c2
cristianvia <cristianviar@gmail.com>
2022-01-21 10:38:00 +0100

    Add images and custom home

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -37,16 +37,45 @@ export default function Home() {
               </Link>
             </div>
             <div class="chibi_playing">
-            <img src="../../images/chibi_playing.png" alt="chibi playing classbits" height="250"/>
+              <img src="../../images/chibi_playing.png" alt="chibi playing classbits" height="250" />
             </div>
-          </div>  
+          </div>
         </div>
         <div class="container cta">
-            <div class="center">
-              <blockquote>Els jocs i la gamificació condueixen a alts nivells de compromís i motivació de l'alumne. - Karl Kapp -
+          <div class="center">
+            <blockquote>Els jocs i la gamificació condueixen a alts nivells de compromís i motivació de l'alumne. - Karl Kapp -
               </blockquote>
           </div>
+        </div>
+        <div style={{ padding: 50 }}>
+          <div class="row">
+            <div class="column" id="chibi_programmer">
+              <div class="upAndDown">
+                <img src="../../images/chibi_programmer.png" alt="Programador chibi" width="500px" />
+              </div>
+            </div>
+            <div class="column">
+              <p class="title">La PRIVACITAT, un dels objectius principals</p>
+              <p class="subtitle">Entenem que les dades dels alumnes són molt sensitives i per això aquesta app ha estat específicament dissenyada per garantir la privacitat.</p>
+              <p className="subtitle">Cap dada sortirà MAI del teu ordinador, tot es queda guardat a la memòria del navegador web.</p>
+            </div>
           </div>
+        </div>
+        <div style={{ padding: 50 }}>
+          <div class="row">
+            <div class="column">
+              <p class="title">App de gestió tutorial configurable</p>
+              <p class="subtitle">Al ClassBits podràs afegir, editar i esborrar alumnes, conductes positives, negatives i bescanvis de punts.</p>
+              <p className="subtitle">Tu decideixes quines conductes sumen punts als Chibits i quines en resten...</p>
+              <p className="subtitle">Inclús pots escollir com bescanviar els punts positius per premis a la vida real... o dins del classbits aconseguint una Mascota Bits!</p>
+            </div>
+            <div class="column" id="chibiCustom">
+              <div style={{ padding: 50 }}>
+                <img src="../../images/custom_classroom.png" alt="afegir mascotes" width="500px" />
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
