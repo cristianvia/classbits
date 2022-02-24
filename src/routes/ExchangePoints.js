5c075baad5e82dddd95361bf19fc09800da63d8a
cristianvia <cristianviar@gmail.com>
2022-02-24 10:51:38 +0100

    Populate select list from json data

--- src/routes/ExchangePoints.js
+++ src/routes/ExchangePoints.js
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import Header from '../Components/Header/Header';
 
 
 const ExchangePoints = () => {
+    var classroom = JSON.parse(localStorage.getItem("classroom") || []);
+
+    useEffect(() => {
+        for(var x in classroom ){
+            var sel = document.createElement("option");
+            sel.innerHTML = classroom[x].name + " " + classroom[x].surname;
+            sel.value  = classroom[x].id;
+            document.getElementById("sender").appendChild(sel);
+          }
+          
+          for(var x in classroom ){
+            var sel = document.createElement("option");
+            sel.innerHTML = classroom[x].name + " " + classroom[x].surname;
+            sel.value  = classroom[x].id;
+            document.getElementById("receiver").appendChild(sel);
+          }
+    }, []);
+    
+
     return (
         <>
-        <Header />
-        <div>
-            Hola
-        </div>
-        </>       
+            <Header />
+            <div>
+                <select id="sender">
+                    <option value="">Donar punts</option>
+                </select>
+
+                <input type="number"></input>
+
+                <select id="receiver">
+                    <option value="">Rebre punts</option>
+                </select>
+                <button>
+                    Fer transferència
+                </button>
+
+            </div>
+        </>
     )
 }
 
