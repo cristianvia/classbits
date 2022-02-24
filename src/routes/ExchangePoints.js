ba518ec4d152878e4cecb4b97b1138769a89a426
cristianvia <cristianviar@gmail.com>
2022-02-24 10:36:00 +0100

    Create exchange points component

---
+++ src/routes/ExchangePoints.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../Components/Header/Header';
+
+
+const ExchangePoints = () => {
+    return (
+        <>
+        <Header />
+        <div>
+            Hola
+        </div>
+        </>       
+    )
+}
+
+export default ExchangePoints
